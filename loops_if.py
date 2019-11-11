3c0d3d42e9fc5f83a7084c94b604b147045cca49
Ubuntu <ubuntu@ip-172-31-24-146.ec2.internal>
2019-11-11 05:16:50 +0000

    send file to git hub

---
+++ loops_if.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created by : Mariam Hemdan
+# Created on : November 2019
+# This program using a for loop and one if statement
+
+
+def main():
+    # this program uses for loop and one if statement
+
+    counter = 0
+
+    # process & output
+    for counter in range(1000, 2001):
+        if counter % 5 == 0:
+            print(counter, end="  ")
+        if counter % 5 == 0:
+
+
+if __name__ == "__main__":
+    main()
